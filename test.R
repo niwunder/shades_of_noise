95017cfb44dabdc16a1160c46e0d22b61c4fb8ed
niwunder <104715287+niwunder@users.noreply.github.com>
2022-05-18 12:47:41 +0200

    commit to push to github

--- test.R
+++ test.R
@@ -1,1 +1,13 @@
 #this is a test
+
+
+library(usethis)
+
+
+
+edit_r_environ()
+GITHUB_PAT <- "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" #paste and save in .Renviron, restart R
+
+use_github(protocol = "https") #, auth_token = Sys.getenv("GITHUB_PAT"))
+
+#this is an edit
